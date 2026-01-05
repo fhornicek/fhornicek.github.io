e4c0f3c9f1733d07c43a729ac25b5a9a2931fa99
Filip Horníček <filiphornicek@Mac.home>
2026-01-05 19:04:27 +0100

    astrolol

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://filiphornicek.cz',
+  site: 'https://fhornicek.github.io',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
