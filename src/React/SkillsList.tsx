e043044a6e278bdbbd799676ec779dd9d44264ff
Filip Horníček <filiphornicek@Mac.home>
2026-01-05 18:50:44 +0100

    prvni astro commit

---
+++ src/React/SkillsList.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+
+const CategoryIcons = {
+  "Tvorba webových stránek": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 640 512"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+  <path d="M64 96c0-35.3 28.7-64 64-64H512c35.3 0 64 28.7 64 64V352H512V96H128V352H64V96zM0 403.2C0 392.6 8.6 384 19.2 384H620.8c10.6 0 19.2 8.6 19.2 19.2c0 42.4-34.4 76.8-76.8 76.8H76.8C34.4 480 0 445.6 0 403.2zM281 209l-31 31 31 31c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-9.4-9.4-9.4-24.6 0-33.9l48-48c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM393 175l48 48c9.4 9.4 9.4 24.6 0 33.9l-48 48c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l31-31-31-31c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"></path>
+
+</svg>
+  ),
+  "Mobile Development": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+  <path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"></path>
+
+</svg>
+  ),
+  "UI/UX Design": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 576 512"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+  <path d="M32 119.4C12.9 108.4 0 87.7 0 64C0 28.7 28.7 0 64 0c23.7 0 44.4 12.9 55.4 32H456.6C467.6 12.9 488.3 0 512 0c35.3 0 64 28.7 64 64c0 23.7-12.9 44.4-32 55.4V392.6c19.1 11.1 32 31.7 32 55.4c0 35.3-28.7 64-64 64c-23.7 0-44.4-12.9-55.4-32H119.4c-11.1 19.1-31.7 32-55.4 32c-35.3 0-64-28.7-64-64c0-23.7 12.9-44.4 32-55.4V119.4zM456.6 96H119.4c-5.6 9.7-13.7 17.8-23.4 23.4V392.6c9.7 5.6 17.8 13.7 23.4 23.4H456.6c5.6-9.7 13.7-17.8 23.4-23.4V119.4c-9.7-5.6-17.8-13.7-23.4-23.4zM128 160c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32H160c-17.7 0-32-14.3-32-32V160zM256 320h32c35.3 0 64-28.7 64-64V224h64c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V320z"></path>
+
+</svg>
+  ),
+};
+
+const SkillsList = () => {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
+  const skills = {
+    "Tvorba webových stránek": [
+      "Weby pro podnikání a prodej",
+      "Portfolio weby a online vizitky",
+      "Redesign a optimalizace",
+    ],
+    "Mobile Development": [
+      "Responzivní webové aplikace",
+      "Mobilní aplikace v React Native",
+    ],
+    "UI/UX Design": [
+      "UI design ve Figmě",
+      "UX zlepšení",
+      "Interaktivní prototypy",
+    ],
+  };
+
+  const toggleItem = (item: string) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
+  return (
+    <div className="text-left pt-3 md:pt-9">
+      <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
+        Co nabízím?
+      </h3>
+      <ul className="space-y-4 mt-4 text-lg">
+        {Object.entries(skills).map(([category, items]) => (
+          <li key={category} className="w-full">
+            <div
+              onClick={() => toggleItem(category)}
+              className="md:w-[400px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+            >
+              <div className="flex items-center gap-3 p-4">
+                {CategoryIcons[category]}
+                <div className="flex items-center gap-2 flex-grow justify-between">
+                  <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
+                    <span className="block truncate text-[var(--white)] text-lg">
+                      {category}
+                    </span>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`w-6 h-6 text-[var(--white)] transform transition-transform flex-shrink-0 ${
+                      openItem === category ? "rotate-180" : ""
+                    }`}
+                  >
+                    <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div
+                className={`transition-all duration-300 px-4 ${
+                  openItem === category
+                    ? "max-h-[500px] pb-4 opacity-100"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <ul className="space-y-2 text-[var(--white-icon)] text-sm">
+                  {items.map((item, index) => (
+                    <div key={index} className="flex items-center">
+                      <span className="pl-1">•</span>
+                      <li className="pl-3">{item}</li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillsList;
